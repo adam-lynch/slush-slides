365b30fd9ea46be1fa0fe6ab12d633e5147bac1f
Adam Lynch <contact@adamlynch.ie>
2014-05-04 16:21:52 +0100

    Converted gulpfile to slushfile

--- slushfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('default', function (done) {
-    gulp.src(__dirname + '/src/**')
-        .pipe(gulp.dest('./'))
-        .on('end', function () {
-            done();
-        });
-});
