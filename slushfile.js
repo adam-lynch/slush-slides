e665858e2788c787d2c3829e7620f8a32ae1f3e7
john-webb <johngwebb64@gmail.com>
2014-05-18 17:22:15 +0100

    added gulp install

--- slushfile.js
+++ slushfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('default', ['init']);
 
 gulp.task('init', function(done){
     gulp.src(__dirname + '/template/**', {dot: true})
         .pipe(gulp.dest('./'))
+        .pipe($.install())
         .on('end', function () {
             done();
         });
