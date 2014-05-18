a4409967c2f097422dcc6c00e83b210031ccc162
Adam Lynch <contact@adamlynch.ie>
2014-05-18 16:57:48 +0100

    Fix for .bowerrc not being copied

--- slushfile.js
+++ slushfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 gulp.task('default', function(done){
-    gulp.src(__dirname + '/template/**')
+    gulp.src(__dirname + '/template/**', {dot: true})
         .pipe(gulp.dest('./'))
         .on('end', function () {
             done();
