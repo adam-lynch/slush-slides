81543c2977045982f351c08759ebd94106c9e403
Adam Lynch <contact@adamlynch.ie>
2014-05-18 17:04:27 +0100

    Added slush 'init' task

--- slushfile.js
+++ slushfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 
-gulp.task('default', function(done){
+gulp.task('default', ['init']);
+
+gulp.task('init', function(done){
     gulp.src(__dirname + '/template/**', {dot: true})
         .pipe(gulp.dest('./'))
         .on('end', function () {
