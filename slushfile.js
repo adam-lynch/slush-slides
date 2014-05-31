0ae1f3964c6fb4fc7c0833b31f3feaeb31b81c49
Adam Lynch <contact@adamlynch.ie>
2014-05-31 12:19:22 +0100

    Small slushfile change

--- slushfile.js
+++ slushfile.js
@@ -9,7 +9,5 @@ gulp.task('init', function(done){
     gulp.src(__dirname + '/template/**', {dot: true})
         .pipe(gulp.dest('./'))
         .pipe($.install())
-        .on('end', function () {
-            done();
-        });
+        .on('end', done);
 });
